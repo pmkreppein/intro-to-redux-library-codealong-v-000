9088f8107595006c0961f2d9f44bc32d24fac37a
Peter Kreppein <pmkreppein@gmail.com>
2019-01-29 20:54:06 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -7,5 +7,7 @@ import shoppingListItemReducer from './reducers/shoppingListItemReducer.js';
 import App from './App';
 import './index.css';
 import { createStore } from 'redux';
+const store = createStore(shoppingListItemReducer,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
 
 ReactDOM.render(<App />, document.getElementById('root'));
